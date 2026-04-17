ecb8bff70a83f2870905cf0047a805006cf5f3f0
Ridho Satria Wibawa <ridhosatria@students.amikom.ac.id>
2026-04-17 08:44:16 +0700

    Tugas 3

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
-});
+// Route::get('/', function () {
+//     return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
+// });
 
-Route::get('/kontak', function () {
- return view('contact');
-});
+// Route::get('/kontak', function () {
+//  return view('contact');
+// });
 
-Route::get('/profil', function () {
- return view('profil');
-});
+// Route::get('/profil', function () {
+//  return view('profil');
+// });
 
-Route::get('/katalog', function () {
- return view('katalog');
-});
+// Route::get('/katalog', function () {
+//  return view('katalog');
+// });
 
-Route::get('/bantuan', function () {
- return view('bantuan');
+// Route::get('/bantuan', function () {
+//  return view('bantuan');
+// });
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
 });
